bcbebece52b5264ca889dfde039c1a8d563c4c58
Ronald Deguzman <bootcampcoding.ronnie@gmail.com>
2018-07-22 13:51:21 -0700

    halfway done editing html of inquire form - still need to incorporate built in javascript

--- src/components/Inquire/Inquire.js
+++ src/components/Inquire/Inquire.js
@@ -5,6 +5,8 @@ const Inquire = () =>
     <div className="container">
         <form class="needs-validation" novalidate>
             <div class="form-row">
+
+
                 <div class="col-md-4 mb-3">
                     <label for="validationCustom01">First name</label>
                     <input type="text" class="form-control"
@@ -27,6 +29,76 @@ const Inquire = () =>
                 </div>
             </div>
         </form>
+
+        {/* May need this  */}
+        {/* <div class="col-md-4 mb-3">
+            <label for="validationCustomUsername">Username
+                      </label>
+            <div class="input-group">
+                <div class="input-group-prepend">
+                    <span class="input-group-text" id="inputGroupPrepend">@</span>
+                </div>
+            </div>
+        </div> */}
+
+        {/* May need this  */}
+            {/* <input type="text" class="form-control"                     id="validationCustomUsername"                           placeholder="Username"                                  aria-describedby="inputGroupPrepend" required>
+            </input>
+                 <div class="invalid-feedback">
+                    Please choose a username.
+                </div> */}
+
+        {/* </div> */}
+
+
+                <div class="form-row">
+                    <div class="col-md-6 mb-3">
+                        <label for="validationCustom03">City</label>
+                            <input type="text" class="form-control" id="validationCustom03" placeholder="City" required>
+                            </input>
+                                <div class="invalid-feedback">
+                                     Please provide a valid city.
+                                </div>
+                    </div>
+                </div>
+
+
+
+                <div class="col-md-3 mb-3">                             <label for="validationCustom04">State</label>            <input type="text" class="form-control"                id="validationCustom04"                             placeholder="State" required>
+                        </input>                            
+                            <div class="invalid-feedback">            Please provide a valid st
+                            </div> 
+                </div>
+
+                 <div class="col-md-3 mb-3">
+                    <label for="validationCustom05">Zip</label>
+                        <input type="text" class="form-control" id="validationCustom05" placeholder="Zip" required>
+                        </input>
+                            <div class="invalid-feedback">
+                                Please provide a valid zip.
+                            </div>
+                    </div>
+
+
+
+                <div class="form-group">                                <div class="form-check">                                <input class="form-check-input"                     type="checkbox" value="" id="invalidCheck"          required></input>                                   <label class="form-check-label"                      for="invalidCheck">                                  Agree to terms and conditions
+                        </label>
+
+                        <div class="invalid-feedback">                You must agree before submitting.   
+                        </div> 
+                    </div>                                     
+                </div>
+                    <button class="btn btn-primary" type="submit">Submit form
+                    </button>
+
+
+
+
+
+
+
+
+
 
 
 
@@ -54,71 +126,20 @@ const Inquire = () =>
 
 
 
-//                     <div class="col-md-4 mb-3">
-//                         <label for="validationCustomUsername">Username</label>
-//                         <div class="input-group">
-//                             <div class="input-group-prepend">
-//                                 <span class="input-group-text" id="inputGroupPrepend">@</span>
-//                             </div>
-//                             <input type="text" class="form-control" id="validationCustomUsername" placeholder="Username" aria-describedby="inputGroupPrepend" required>
-//                                 <div class="invalid-feedback">
-//                                     Please choose a username.
-//     </div>
-//   </div>
-//                         </div>
-//                     </div>
-
-
-//                     <div class="form-row">
-//                         <div class="col-md-6 mb-3">
-//                             <label for="validationCustom03">City</label>
-//                             <input type="text" class="form-control" id="validationCustom03" placeholder="City" required>
-//                                 <div class="invalid-feedback">
-//                                     Please provide a valid city.
-//   </div>
-// </div>
-
-
-
-//                             <div class="col-md-3 mb-3">
-//                                 <label for="validationCustom04">State</label>
-//                                 <input type="text" class="form-control" id="validationCustom04" placeholder="State" required>
-//                                     <div class="invalid-feedback">
-//                                         Please provide a valid state.
-//   </div>
-// </div>
 
 
 
 
-//                                 <div class="col-md-3 mb-3">
-//                                     <label for="validationCustom05">Zip</label>
-//                                     <input type="text" class="form-control" id="validationCustom05" placeholder="Zip" required>
-//                                         <div class="invalid-feedback">
-//                                             Please provide a valid zip.
-//   </div>
-// </div>
 
 
 
-//                                 </div>
-//                                 <div class="form-group">
-//                                     <div class="form-check">
-//                                         <input class="form-check-input" type="checkbox" value="" id="invalidCheck" required>
-//                                             <label class="form-check-label" for="invalidCheck">
-//                                                 Agree to terms and conditions
-//   </label>
-
-
-//                                             <div class="invalid-feedback">
-//                                                 You must agree before submitting.
-//   </div>
-// </div>
-//                                     </div>
 
 
 
-// <button class="btn btn-primary" type="submit">Submit form</button>
+
+
+
+
 
 
 
