b14e218f6160eea1701ba2fb1f68a12aef511d67
Ronald Deguzman <bootcampcoding.ronnie@gmail.com>
2018-07-24 12:19:43 -0700

    succesfully created inquire page with responsive fields for user input on contact info and a message box to send to user

--- src/components/Interact/Interact.js
+++ src/components/Interact/Interact.js
@@ -24,9 +24,139 @@ const Interact = () =>
 
             <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal" data-whatever="@mdo">Inquire
             </button>
+            <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div class="modal-dialog" role="document">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h5 class="modal-title" id="exampleModalLabel">Contact Information</h5>
+                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                <span aria-hidden="true">&times;</span>
+                            </button>
+                        </div>
+                        <div class="modal-body">
 
-        
+
+                        <form class="needs-validation" novalidate>
+                        <div class="form-row">
+
+
+                            <div class="col-md-3 mb-3">
+                                <label for="validationCustom01">First Name
+                        </label>
+                                <input type="text" class="form-control" id="validationCustom01" placeholder="First Name" required>
+                                </input>
+
+                                <div class="invalid-feedback">
+                                Please provide a valid first name.
+                        </div>
+                            </div>
+
+
+                            <div class="col-md-3 mb-3">
+                                <label for="validationCustom02">Last Name</label>
+                                <input type="text" class="form-control" id="validationCustom02" placeholder="Last Name" required>
+                                </input>
+
+                                <div class="invalid-feedback">
+                                Please provide a valid email
+                        </div>
+                            </div>
+
+                        </div>
+                    </form>
+
+
+                    <form class="needs-validation" novalidate>
+                        <div class="form-row">
+
+
+                            <div class="col-md-3 mb-3">
+                                <label for="validationCustom03">Phone Number
+                        </label>
+                                <input type="text" class="form-control" id="validationCustom03" placeholder="XXX-XXX-XXX" required>
+                                </input>
+
+                                <div class="invalid-feedback">Please provide a valid phone number.
+                        </div>
+                            </div>
+
+
+                            <div class="col-md-3 mb-3">
+                                <label for="validationCustom04">e-mail</label>
+                                <input type="text" class="form-control" id="validationCustom04" placeholder="e-mail" required>
+                                </input>
+
+                                <div class="invalid-feedback">            Please provide a valid email
+                        </div>
+                            </div>
+                        </div>
+                    </form>
+
+
+
+                    <form class="needs-validation" novalidate>
+                        <div class="form-row">
+
+
+                            <div class="col-md-3 mb-3">
+                                <label for="validationCustom05">City
+                        </label>
+                                <input type="text" class="form-control" id="validationCustom05" placeholder="City" required>
+                                </input>
+
+                                <div class="invalid-feedback">        Please provide a valid city.
+                        </div>
+                            </div>
+
+
+                            <div class="col-md-3 mb-3">
+                                <label for="validationCustom06">State</label>
+                                <input type="text" class="form-control" id="validationCustom06" placeholder="State" required>
+                                </input>
+
+                                <div class="invalid-feedback">            Please provide a valid st
+                        </div>
+                            </div>
+
+                            <div class="col-md-3 mb-3">
+                                <label for="validationCustom07">Zip</label>
+                                <input type="text" class="form-control" id="validationCustom07" placeholder="Zip" required>
+                                </input>
+                                <div class="invalid-feedback">
+                                    Please provide a valid zip.
+                             </div>
+                            </div>
+                        </div>
+                    </form>
+
+
+                            <form>
+                                <div class="form-group">
+
+
+                                    {/* <label for="recipient-name" class="col-form-label">Recipient:</label>
+                                    <input type="text" class="form-control" id="recipient-name">
+                                    </input> */}
+
+
+                                    <div class="form-group">
+                                        <label for="message-text" class="col-form-label">Message:</label>
+                                        <textarea class="form-control" id="message-text"></textarea>
+                                    </div>
+                                </div>
+                            </form>
+
+                            <div class="modal-footer">
+                                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                                <button type="button" class="btn btn-primary">Send message to owner</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
+
+
 
         <div className="col-1">
         </div>
