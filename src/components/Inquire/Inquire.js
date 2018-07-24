4c6347dc202d0599ad352582621f8cc6547845ff
Ronald Deguzman <bootcampcoding.ronnie@gmail.com>
2018-07-23 19:55:01 -0700

    fixed First name and Last name input boxes to enter characters + modified confirm button to a modal pop up that verifies confirmation of renting item

--- src/components/Inquire/Inquire.js
+++ src/components/Inquire/Inquire.js
@@ -2,103 +2,141 @@ import React from "react";
 import "./Inquire.css"
 const Inquire = () =>
 
-    <form>
-        <div className="container">
-            <form class="needs-validation" novalidate>
-                <div class="form-row">
-
-                    <div class="col-md-4 mb-3">
-                        <label for="validationCustom01">First name</label>
-                        <input type="text" class="form-control"
-                            id="validationCustom01" placeholder="First name" value="Your First Name" required>
-                        </input>
-                        <div class="valid-feedback">
-                            Looks good!
-                </div>
-                    </div>
+    <div>
 
 
-                    <div class="col-md-4 mb-3">
-                        <label for="validationCustom02">Last Name</label>
-                        <input type="text" class="form-control"
-                            id="validationCustom02" placeholder="First name" value="Your Last Name" required>
-                        </input>
-                        <div class="valid-feedback">
-                            Looks good!
-                </div>
-                    </div>
-                </div>
-            </form>
-        </div>
+       <div className="container">
+                    <form class="needs-validation" novalidate>
+                        <div class="form-row">
 
 
-
-        <div className="container">
-            <form class="needs-validation" novalidate>
-                <div class="form-row">
-
-
-                    <div class="col-md-3 mb-3">
-                        <label for="validationCustom03">City
+                            <div class="col-md-3 mb-3">
+                                <label for="validationCustom01">First Name
                         </label>
-                        <input type="text" class="form-control" id="validationCustom03" placeholder="City" required>
-                        </input>
+                                <input type="text" class="form-control" id="validationCustom01" placeholder="First Name" required>
+                                </input>
 
-                        <div class="invalid-feedback">        Please provide a valid city.
+                                <div class="invalid-feedback">
+                                Please provide a valid first name.
                         </div>
-                    </div>
+                            </div>
 
 
-                    <div class="col-md-3 mb-3">
-                        <label for="validationCustom04">State</label>
-                        <input type="text" class="form-control" id="validationCustom04" placeholder="State" required>
-                        </input>
+                            <div class="col-md-3 mb-3">
+                                <label for="validationCustom02">Last Name</label>
+                                <input type="text" class="form-control" id="validationCustom02" placeholder="Last Name" required>
+                                </input>
 
-                        <div class="invalid-feedback">            Please provide a valid st
+                                <div class="invalid-feedback">
+                                Please provide a valid email
                         </div>
-                    </div>
+                            </div>
 
-                    <div class="col-md-3 mb-3">
-                        <label for="validationCustom05">Zip</label>
-                        <input type="text" class="form-control" id="validationCustom05" placeholder="Zip" required>
-                        </input>
-                        <div class="invalid-feedback">
-                            Please provide a valid zip.
-                             </div>
-                    </div>
-
-
+                        </div>
+                    </form>
                 </div>
-            </form>
-        </div>
 
 
-         <div className="container">
-            <form class="needs-validation" novalidate>
-                <div class="form-row">
-            <div class="col-md-3 mb-3">
-                {/* <div class="col-md-3 mb-3"> */}
-                    <input class="form-check-input" type="checkbox" value="" id="invalidCheck" required></input>            <label class="form-check-label" for="invalidCheck">
-                    Agree to terms and conditions                  
+                <div className="container">
+                    <form class="needs-validation" novalidate>
+                        <div class="form-row">
+
+
+                            <div class="col-md-3 mb-3">
+                                <label for="validationCustom03">Phone Number
+                        </label>
+                                <input type="text" class="form-control" id="validationCustom03" placeholder="XXX-XXX-XXX" required>
+                                </input>
+
+                                <div class="invalid-feedback">Please provide a valid phone number.
+                        </div>
+                            </div>
+
+
+                            <div class="col-md-3 mb-3">
+                                <label for="validationCustom04">e-mail</label>
+                                <input type="text" class="form-control" id="validationCustom04" placeholder="e-mail" required>
+                                </input>
+
+                                <div class="invalid-feedback">            Please provide a valid email
+                        </div>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+
+
+
+                <div className="container">
+                    <form class="needs-validation" novalidate>
+                        <div class="form-row">
+
+
+                            <div class="col-md-3 mb-3">
+                                <label for="validationCustom05">City
+                        </label>
+                                <input type="text" class="form-control" id="validationCustom05" placeholder="City" required>
+                                </input>
+
+                                <div class="invalid-feedback">        Please provide a valid city.
+                        </div>
+                            </div>
+
+
+                            <div class="col-md-3 mb-3">
+                                <label for="validationCustom06">State</label>
+                                <input type="text" class="form-control" id="validationCustom06" placeholder="State" required>
+                                </input>
+
+                                <div class="invalid-feedback">            Please provide a valid st
+                        </div>
+                            </div>
+
+                            <div class="col-md-3 mb-3">
+                                <label for="validationCustom07">Zip</label>
+                                <input type="text" class="form-control" id="validationCustom07" placeholder="Zip" required>
+                                </input>
+                                <div class="invalid-feedback">
+                                    Please provide a valid zip.
+                             </div>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+
+
+                <div className="container">
+                    <form class="needs-validation" novalidate>
+                        <div class="form-row">
+                            <div class="col-md-3 mb-3">
+                                {/* <div class="col-md-3 mb-3"> */}
+                                <input class="form-check-input" type="checkbox" value="" id="invalidCheck" required></input>            <label class="form-check-label" for="invalidCheck">
+                                    Agree to terms and conditions
                     </label>
-                    <div class="invalid-feedback">
-                    You must agree before submitting.        
+                                <div class="invalid-feedback">
+                                    You must agree before submitting.
                     </div>
-                {/* </div> */}
-            </div>
-        </div>
-        </form>
+                                {/* </div> */}
+                            </div>
+                        </div>
+                    </form>
 
-        <button class="btn btn-primary" type="submit">Submit form
+                    <div className="text-box">
+                        <textarea name="message" rows="7" cols="60">
+                            Enter Message
+                </textarea>
+                    </div>
+
+                    <button class="btn btn-primary" type="submit">Submit
             </button>
+                </div>
             </div>
-    </form >
 
-export default Inquire;
-
-
-
-
+            export default Inquire;
+            
+            
+            
+            
 {/* <div class="form-group">                                <div class="form-check">                                <input class="form-check-input" type="checkbox" value="" id="invalidCheck" required></input>                                   <label class="form-check-label" for="invalidCheck">                                  Agree to terms and conditions
                         </label>
 
@@ -110,14 +148,14 @@ export default Inquire;
                     </button> */}
 
 
-{/* </div> */ }
+            {/* </div> */}
 
 
-{/* //Example starter JavaScript for disabling form submissions if there are invalid fields */ }
+            {/* //Example starter JavaScript for disabling form submissions if there are invalid fields */}
 
-{/* <script> */ }
+            {/* <script> */}
 
-{/* (function() {
+            {/* (function() {
     'use strict';                                       window.addEventListener('load', function() {
 // Fetch all the forms we want to apply custom Bootstrap validation styles to
     let forms = document.getElementsByClassName('needs-validation');
@@ -133,6 +171,6 @@ export default Inquire;
                                          });
                                        }, false);
                                      })(); */}
-{/* </script> */ }
+            {/* </script> */}
 
 
--- src/components/Interact/Interact.js
+++ src/components/Interact/Interact.js
@@ -7,6 +7,11 @@ const Interact = () =>
 
     // <div className="mx-auto">
 
+
+
+
+
+
     <div className="d-flex justify-content-center" id="interact-box">
         {/* <a className="p-3" href="">Inquire</a> */}
 
@@ -20,7 +25,7 @@ const Interact = () =>
         </div> */}
 
         <div className="col-3">
-        <a className="btn btn-primary btn-sm" id="inquire-btn" href="">Inquire</a>
+            <a className="btn btn-primary btn-md" id="inquire-btn" href="">Inquire</a>
         </div>
 
         <div className="col-1">
@@ -28,31 +33,147 @@ const Interact = () =>
 
 
         <div className="col-2">
-        <a className="btn btn-primary btn-sm" id="view-btn" href="">View Reviews</a>
+            <a className="btn btn-primary btn-md" id="view-btn" href="">View Reviews</a>
         </div>
 
         <div className="col-1">
         </div>
 
         <div className="col-2">
-        <a className="btn btn-primary btn-sm" href="">Confirm</a>
+         {/* <div className="btn btn-primary btn-sm" href=""></div> */}
 
+
+
+         {/* <!-- Button trigger modal --> */}
+        <button type="button" class="btn btn-primary" id="confirm-btn" data-toggle="modal" data-target="#exampleModal">
+            Confirm
+        </button>
+
+        {/* <!-- Modal --> */}
+        <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog" role="document">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="exampleModalLabel">Confirm Rental</h5>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+            <div class="modal-body">
+                        Clicking "Ok" will confirm your rental. 
+            </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
+                        <button type="button" class="btn btn-primary">Ok</button>
+                    </div>
+                </div>
+            </div>
         </div>
+
+
+       </div>
+    
 
         <div className="col-1">
         </div>
 
         <div className="col-1">
         </div>
-
-        {/* <div className="col-1">
-        </div> */}
-
-
-        {/* <a className="btn btn-primary btn-sm" href="">Inquire</a>
-        <a className="btn btn-primary btn-sm" href="">View Reviews</a>
-        <a className="btn btn-primary btn-sm" href="">Confirm</a> */}
     </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// <div className="d-flex justify-content-center" id="interact-box">
+//     {/* <a className="p-3" href="">Inquire</a> */}
+
+//     {/* <div className="col-2">
+//     </div> */}
+
+//     <div className="col-1">
+//     </div>
+
+//     {/* <div className="col-1">
+//     </div> */}
+
+//     <div className="col-3">
+//     <a className="btn btn-primary btn-sm" id="inquire-btn" href="">Inquire</a>
+//     </div>
+
+//     <div className="col-1">
+//     </div>
+
+
+//     <div className="col-2">
+//     <a className="btn btn-primary btn-sm" id="view-btn" href="">View Reviews</a>
+//     </div>
+
+//     <div className="col-1">
+//     </div>
+
+//     <div className="col-2">
+//     <a className="btn btn-primary btn-sm" href="">Confirm</a>
+
+//     </div>
+
+//     <div className="col-1">
+//     </div>
+
+//     <div className="col-1">
+//     </div>
+
+//     {/* <div className="col-1">
+//     </div> */}
+
+
+//     {/* <a className="btn btn-primary btn-sm" href="">Inquire</a>
+//     <a className="btn btn-primary btn-sm" href="">View Reviews</a>
+//     <a className="btn btn-primary btn-sm" href="">Confirm</a> */}
+// </div>
 
 
 
