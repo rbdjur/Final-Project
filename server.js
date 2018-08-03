f2deba52da14c1b7c03f75c023d99979e3c88a8a
Ronald Deguzman <bootcampcoding.ronnie@gmail.com>
2018-08-02 19:00:55 -0700

    combined backend server and socket.io server onto one server.  ran the server and it worked with persisting data when web application closed and when another tab opened with web application.However, pretty sure this will work on heroku but be skeptical bc backend plus frontend server combined

--- server.js
+++ server.js
@@ -1,44 +1,32 @@
-const mongoose = require("mongoose");
-const axios = require("axios");
-
-const express = require("express");
-const app = express();
+// const mongoose = require("mongoose");
+// const axios = require("axios");
 
 
-const bodyParser = require("body-parser");
+// const bodyParser = require("body-parser");
 // defining router and assigning it to require the ConfirmationRoutes.js page enables use to use what is on that page
-const router = require("./routes/ConfirmationRoutes")
+// const router = require("./routes/ConfirmationRoutes")
 // Define port
-const PORT = 3001;
 // Code below is alternate of code above
 // const PORT = process.env.PORT || 3000
 
-// Code Below are variables for socket.io
-// var socket = require('express')();
-// const http = require('http').Server(app);
-// const io = require('socket.io')(http);
-
-
-
 
 // Mongoose connection
-const db  = mongoose.connection; 
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function() {
-    console.log("we are connected to mongodb - using FinalProject database.")
-});
+// const db  = mongoose.connection; 
+// db.on('error', console.error.bind(console, 'connection error:'));
+// db.once('open', function() {
+//     console.log("we are connected to mongodb - using FinalProject database.")
+// });
 
 // Use body-parser for handling form submissions
-app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(bodyParser.urlencoded({ extended: true }));
 
 // Set up a static folder (public) for our web app
-app.use(express.static("public"));
 
-app.use(router);
+// app.use(router);
 
 // For test reasons, we utilize test database - use cities collections - inside should be city berkeey and attractions.
 // Make sure to run "mongod" inside terminal to turn on mongo database
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/FinalProject");
+// mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/FinalProject");
 
 // Alernate code of the code above connect to database 
 // // Connect to the Mongo DB
@@ -49,19 +37,30 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/FinalProject");
 
 // put socket.io into separate "project" b/c limitations to heroku.
 // CORS - check for configuration to enable CORS to prevent error.
-const socket = require('socket.io');
-const socketPORT = 3002
 
-// socket.io server socket
-server = app.listen(socketPORT, function (err) {
-  if (err) throw err
-  console.log('listening to socketPORT on port: ', socketPORT);
+
+// // socket.io server socket
+// server = app.listen(socketPORT, function (err) {
+//   if (err) throw err
+//   console.log('listening to socketPORT on port: ', socketPORT);
+// })
+
+const express = require("express");
+const app = express();
+const socket = require('socket.io');
+const PORT = 3001;
+app.use(express.static("public"));
+// const socketPORT = 3002;
+
+// start the server.js
+const server = app.listen(PORT, function() {
+  console.log("server.js for App.js is running on port " + PORT);
 })
 
-io = socket(server);
+
+const io = socket(server);
 
 const messageLog = [];
-
 // initlization of socket
 io.on('connection', (socket) => {
   console.log("Inside io.on.connection - returning socket.id: ", socket.id);
@@ -74,8 +73,3 @@ io.on('connection', (socket) => {
   })
 });
 
-
-// start the server.js
-app.listen(PORT, function() {
-    console.log("server.js for App.js is running on port " + PORT);
-})
